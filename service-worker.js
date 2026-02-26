64215bb822d16cc82786549e511b95554bd6a954
Davofn <davofn@gmail.com>
2026-02-26 01:12:32 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "r5-consumo-log-v1-8";
+const CACHE_NAME = "r5-consumo-log-v1-9";
 const ASSETS = [
   "./",
   "./index.html",
@@ -33,4 +33,5 @@ self.addEventListener("fetch", (event) => {
   );
 
 });
+
 
